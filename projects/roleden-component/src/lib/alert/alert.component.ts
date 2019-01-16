8abd9b35449b1b381d3fc8ad64b44966fddf93b8
Thibaud Huber <thibaud.huber@epitech.eu>
2019-01-16 10:14:33 -0500

    Alert Animations

--- projects/roleden-component/src/lib/alert/alert.component.ts
+++ projects/roleden-component/src/lib/alert/alert.component.ts
@@ -1,11 +1,28 @@
 import { Alert } from './alert.class';
 import { Component, OnInit } from '@angular/core';
 import { AlertService } from './alert.service';
+import { trigger, transition, query, stagger, animate, keyframes, style } from '@angular/animations';
 
 @Component({
   selector: 'rd-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.scss']
+  styleUrls: ['./alert.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', stagger('300ms', [
+          animate('.5s ease-in', keyframes([
+            style({opacity: 0.2, transform: 'translateX(75%)', offset: 0}),
+            style({opacity: 1, transform: 'translateX(0)',     offset: 1.0}),
+          ]))]), {optional: true}),
+          query(':leave', stagger('300ms', [
+            animate('.5s ease-in', keyframes([
+              style({opacity: 1, transform: 'translateX(0)', offset: 0}),
+              style({opacity: 0, transform: 'translateX(100%)',     offset: 1.0}),
+            ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class AlertComponent implements OnInit {
 
