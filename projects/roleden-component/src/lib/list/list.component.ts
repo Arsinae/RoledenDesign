d816062b15ac155f08192ff5b6ab470226f5ac1c
Thibaud Huber <thibaud.huber@epitech.eu>
2019-01-17 00:39:26 -0500

    Lists animation

--- projects/roleden-component/src/lib/list/list.component.ts
+++ projects/roleden-component/src/lib/list/list.component.ts
@@ -1,10 +1,25 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ListElementDirective} from './list-element.directive';
+import { trigger, transition, query, stagger, animate, keyframes, style } from '@angular/animations';
 
 @Component({
   selector: 'rd-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', animate('.3s ease-in', keyframes([
+            style({opacity: 0.2, transform: 'translateY(-50%)', offset: 0}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1.0}),
+          ])), {optional: true}),
+          query(':leave', animate('.3s ease-in', keyframes([
+              style({opacity: 1, offset: 0}),
+              style({opacity: 0, maxHeight: 0, transform: 'translateY(50%)', offset: 1.0}),
+            ])), {optional: true})
+      ])
+    ])
+  ]
 })
 export class ListComponent implements OnInit {
 
