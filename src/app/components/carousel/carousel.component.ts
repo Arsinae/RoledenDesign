b83fa660190f2a833f4d45c4e806d6914265212e
Thibaud Huber <thibaud.huber@epitech.eu>
2019-01-22 10:42:37 -0500

    Code Example Tabs & Carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
+  public htmlCode = '<rd-carousel>\n' +
+  '  <rd-carousel-data>\n    <img src="https://stmed.net/sites/default/files/dune-wallpapers-29516-4438731.jpg">\n  </rd-carousel-data>\n' +
+  '  <rd-carousel-data>\n    <img src="https://i.pinimg.com/originals/9c/65/07/9c65071899c8c179adda73ca7ab237b6.jpg">\n' +
+  '  </rd-carousel-data>\n</rd-carousel>\n';
+
   constructor() { }
 
   ngOnInit() {
--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -9,17 +9,21 @@ export class TabsComponent implements OnInit {
 
   public tabs = ['Leto Atréides', 'Jessica Atréides', 'Paul Atréides', 'Alia Atréides', 'Leto II Atréides', 'Ghanima Atréides'];
 
+  public htmlCode = '<rd-tabs-group [color]="\'purple\'" (displayedIndexChange)="changeIndex($event)">\n' +
+    ' <rd-tabs *ngFor="let title of tabs" [title]="title">\n   <div>{{title}}</div>\n' +
+    ' </rd-tabs>\n</rd-tabs-group>\n';
+
+  public tsCode = 'public tabs = [\n  \'Leto Atréides\',\n  \'Jessica Atréides\',\n' +
+  '  \'Paul Atréides\',\n  \'Alia Atréides\',\n  \'Leto II Atréides\',\n  \'Ghanima Atréides\'\n];\n\n' +
+  'changeIndex(ev) {\n  console.log(\'index change.\', ev);\n}\n';
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  cutTabs() {
-    this.tabs.splice(0, 1);
-  }
-
-  putTabs() {
-    this.tabs.push('a');
+  changeIndex(ev) {
+    console.log('Index change.', ev);
   }
 
 }
