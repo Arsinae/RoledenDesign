18481b0dea191723725d412a0a1eb169493b8b3d
Thibaud Huber <thibaud.huber@epitech.eu>
2019-01-22 10:13:41 -0500

    Accordeon Example

--- src/app/components/accordeon/accordeon.component.ts
+++ src/app/components/accordeon/accordeon.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccordeonComponent implements OnInit {
 
+  public htmlCode = '<rd-accordion>\n <rd-panel [color]="\'red\'">\n' +
+    '   <div class="panelTitle" title>House Atréides</div>\n    <span>One of the Great Houses of ...</span>\n' +
+    ' </rd-panel>\n <rd-panel (stateChange)="openHarkonnen($event)">\n' +
+    '   <div class="panelTitle" title>House Harkonnen</div>\n   <span>The Harkonnens are featured ...</span>\n' +
+    ' </rd-panel>\n</rd-accordion>\n';
+
+    public tsCode = 'openHarkonnen(ev) {\n  console.log(\'Harkonnen Panel opened.\', ev);\n}\n';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openHarkonnen(ev) {
+    console.log('Harkonnen Panel opened.', ev);
   }
 
 }
