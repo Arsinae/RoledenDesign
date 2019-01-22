8926fa9d8fe924e57123b70351efa39d1b201c95
Thibaud Huber <thibaud.huber@epitech.eu>
2019-01-22 10:22:19 -0500

    Explanation Animation

--- src/app/explanation/explanation.component.ts
+++ src/app/explanation/explanation.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { transition, animate, trigger, keyframes, style } from '@angular/animations';
 
 @Component({
   selector: 'app-explanation',
   templateUrl: './explanation.component.html',
-  styleUrls: ['./explanation.component.scss']
+  styleUrls: ['./explanation.component.scss'],
+  animations: [
+    trigger('fadeAnimation', [
+      transition('false => true', [
+        animate('.3s linear', keyframes([
+          style({opacity: 0, offset: 0}),
+          style({opacity: 1, offset: 1.0}),
+        ]))
+      ])
+    ])
+  ]
 })
 export class ExplanationComponent implements OnInit {
 
