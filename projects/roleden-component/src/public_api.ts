c46b201a08f463a20929c7e1c7f59178d5710532
Thibaud Huber <thibaud.huber@epitech.eu>
2019-03-21 21:18:15 -0400

    Package Dependency & Public API

--- projects/roleden-component/src/public_api.ts
+++ projects/roleden-component/src/public_api.ts
@@ -1,16 +1,99 @@
 /*
  * Public API Surface of roleden-component
  */
-
+//    ROLEDEN
 export * from './lib/roleden-component.service';
 export * from './lib/roleden-component.module';
+
+//    ACCORDION
+export * from './lib/accordion/accordion.module';
+export * from './lib/accordion/accordion.component';
+export * from './lib/accordion/panel/panel.component';
+
+//    ALERT
+export * from './lib/alert/alert.module';
+export * from './lib/alert/alert.component';
+export * from './lib/alert/alert.service';
+export * from './lib/alert/alert.class';
+
+//    BREADCRUMB
+export * from './lib/breadcrumb/breadcrumb.module';
+export * from './lib/breadcrumb/breadcrumb.component';
+export * from './lib/breadcrumb/breadcrumb-element.directive';
+
+//    BUTTON
+export * from './lib/button/button.component';
+
+//    CAROUSEL
+export * from './lib/carousel/carousel.module';
+export * from './lib/carousel/carousel.component';
+export * from './lib/carousel/carousel-data/carousel-data.component';
+
+//    CHECK-BOX
+export * from './lib/check-box/check-box.module';
+export * from './lib/check-box/check-box.component';
+export * from './lib/check-box/check-box-option/check-box-option.component';
+
+//    CONFIRM-MODAL
+export * from './lib/confirm-modal/confirm-modal.component';
+
+//    DICE
 export * from './lib/dice/dice.component';
-export * from './lib/small-card/small-card.component';
-export * from './lib/text-editor/text-editor.component';
+
+//    FLIPPING-CARD
+export * from './lib/flipping-card/flipping-card.component';
+
+//    INPUT
+export * from './lib/input/input.component';
+
+//    INPUT-NUMBER
+export * from './lib/input-number/input-number.component';
+
+//    LIST
+export * from './lib/list/list.module';
 export * from './lib/list/list.component';
 export * from './lib/list/list-element.directive';
-export * from './lib/button/button.component';
-export * from './lib/snack-bar/snack-bar.service';
-export * from './lib/alert/alert.service';
+
+//    MODAL
 export * from './lib/modal/modal.component';
-export * from './lib/confirm-modal/confirm-modal.component';
+
+//    MUSIC-PLAYER
+export * from './lib/music-player/music-player.module';
+export * from './lib/music-player/music-player.component';
+export * from './lib/music-player/music-element.directive';
+
+//    NOTATION
+export * from './lib/notation/notation.component';
+
+//    PROGRESS-BAR
+export * from './lib/progress-bar/progress-bar.component';
+
+//    RADIO-BUTTON
+export * from './lib/radio-button/radio-button.module';
+export * from './lib/radio-button/radio-button.component';
+export * from './lib/radio-button/radio-button-option/radio-button-option.component';
+
+//    SELECT
+export * from './lib/select/select.module';
+export * from './lib/select/select.component';
+export * from './lib/select/select-option/select-option.component';
+
+//    SMALL-CARD
+export * from './lib/small-card/small-card.component';
+
+//    SNACK-BAR
+export * from './lib/snack-bar/snack-bar.module';
+export * from './lib/snack-bar/snack-bar.component';
+export * from './lib/snack-bar/snack-bar.service';
+export * from './lib/snack-bar/snack-bar.class';
+
+//    TABS
+export * from './lib/tabs/tabs.module';
+export * from './lib/tabs/tabs.component';
+export * from './lib/tabs/tabs-group/tabs-group.component';
+
+//    TEXT-EDITOR
+export * from './lib/text-editor/text-editor.component';
+
+//    TOOLTIP
+export * from './lib/tooltip/tooltip.component';
