f050169245157083a1ffd9d0eedbe7f6eecb0e54
Thibaud Huber <thibaud.huber@epitech.eu>
2019-04-04 20:10:20 -0400

    Levenshtein distance for Autocomplete Suggestion

--- projects/roleden-component/src/lib/input-autocomplete/input-autocomplete.component.ts
+++ projects/roleden-component/src/lib/input-autocomplete/input-autocomplete.component.ts
@@ -64,13 +64,36 @@ export class RDInputAutocompleteComponent implements OnInit, OnChanges {
   }
 
   getMatchingValues() {
-    const matching = [];
+    const matching = [], levensthein = [];
     for (const value of this.completeValues) {
       if (value.match(this.value)) {
         matching.push(value);
+      } else if (this.levenshteinDistance(this.value, value) <= value.length / 2) {
+        levensthein.push(value);
       }
     }
-    return matching;
+    return matching.concat(levensthein);
+  }
+
+  levenshteinDistance (a: string, b: string): number {
+    const m = [], min = Math.min;
+
+    if (!(a && b)) {
+      return (b || a).length;
+    }
+    for (let i = 0; i <= b.length; m[i] = [i++]) {}
+    for (let j = 0; j <= a.length; m[0][j] = j++) {}
+
+    for (let i = 1; i <= b.length; i++) {
+      for (let j = 1; j <= a.length; j++) {
+        if (b.charAt(i - 1) === a.charAt(j - 1)) {
+        m[i][j] = m[i - 1][j - 1];
+        } else {
+        m[i][j] = m[i][j] = min(m[i - 1][j - 1] + 1, min(m[i][j - 1] + 1, m[i - 1 ][j] + 1));
+        }
+      }
+    }
+    return m[b.length][a.length];
   }
 
   clickValue(value) {
