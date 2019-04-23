86c91cfeedd414d7d10230fde6456bfd9d5d3686
Thibaud Huber <thibaud.huber@epitech.eu>
2019-04-23 13:35:11 -0400

    Two loader Type

--- projects/roleden-component/src/lib/loader/loader.component.ts
+++ projects/roleden-component/src/lib/loader/loader.component.ts
@@ -1,3 +1,4 @@
+import { RDDarkService } from './../dark.service';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
@@ -8,14 +9,19 @@ import { Component, OnInit, Input } from '@angular/core';
 export class RDLoaderComponent implements OnInit {
 
   @Input() width = 120;
-  @Input() height = 120;
   @Input() type = 'circle';
   @Input() color = 'gold';
   @Input() animation = 'linear';
 
-  constructor() { }
+  public dark = false;
+
+  constructor(private darkService: RDDarkService) { }
 
   ngOnInit() {
+    this.darkService.getDarkElement().subscribe(res => {
+      this.dark = this.darkService.isDark();
+    });
+    this.dark = this.darkService.isDark();
   }
 
 }
